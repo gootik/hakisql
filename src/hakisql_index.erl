9db570983708e4961dfc5abd6b079f1636cd029c
Sasan Hezarkhani <gootik_2003@hotmail.com>
2017-10-13 17:49:10 +0100

    Commentary on indexing

--- src/hakisql_index.erl
+++ src/hakisql_index.erl
@@ -4,7 +4,49 @@
     calculate_index_map/3
 ]).
 
+%% @doc To demonstrate the index calculation we can use this example:
+%%      Schema ->
+%%          a => [index]
+%%          b => [index]
+%%          c => []
+%%
+%%      Which means we need to have separate bitmap maps for every value
+%%      of 'a' and 'b'.
+%%
+%%      Here we first create a map of every indexed field, that contains
+%%      all possible value bitmaps. Hence we get:
+%%
+%%      InitMap = #{
+%%          a => #{},
+%%          b => #{}
+%%      }
+%%
+%%      Next, we loop through every row and adding/updating the bitmaps for
+%%      every entry of the indexed fields. Given below example as inserted
+%%      rows:
+%%
+%%      [#{a => test, b = 2, c => not_important},
+%%       #{a => test2, b = 2, c => not_important},
+%%       #{a => test, b = 3, c => not_important}]
+%%
+%%      We _should_ have a final map that looks like:
+%%
+%%      FinalMap = #{
+%%          a => #{
+%%              test  => 101,
+%%              test2 => 010
+%%          },
+%%          b => #{
+%%              2 => 110,
+%%              3 => 001
+%%          }
+%%      }
+%%
+%%      We then cache this map for every field and use the bitmaps at query
+%%      time to figure out which rows need to be fetched.
+%% @end
 calculate_index_map(#{index_field_names := IndexFieldNames} = _Schema, Rows, NumRows) ->
+
     InitMap = lists:foldl(
         fun(Field, Acc) ->
             Acc#{Field => #{}}
